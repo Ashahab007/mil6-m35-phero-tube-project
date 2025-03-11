2a0963cf8921d1f285d3ee4dc4599f7cdf5a3cc0
Ashahab007 <ashahab007@gmail.com>
2025-03-12 02:59:53 +0600

    video section completed

--- script/index.js
+++ script/index.js
@@ -42,21 +42,46 @@ const showVideos = (video) => {
     const videoCard = document.createElement("div");
     videoCard.innerHTML = `
     <div class="card bg-base-100 shadow-sm">
-        <figure>
+    <figure class="relative object-cover">
+      <img class="w-full h-52 object-cover"
+        src="${vids.thumbnail}"
+        alt="Shoes"
+      />
+      <span
+        class="bg-black absolute text-white p-1 rounded-sm bottom-[5px] right-[5px]"
+        >3hrs 56 min ago</span
+      >
+    </figure>
+    <div class="flex gap-6 items-center mt-3">
+      <div class="avatar">
+        <div
+          class="ring-primary ring-offset-base-100 w-8 h-8 rounded-full ring ring-offset-2"
+        >
           <img
-            src="${vids.thumbnail}"
+            class=""
+            src="${vids.authors[0].profile_picture}"
           />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">${vids.title}</h2>
-          <p>
-            ${vids.description}
-          </p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
         </div>
       </div>
+
+      <h2 class="card-title">
+        Building a Winning UX Strategy Using the Kano Model
+      </h2>
+    </div>
+    <div class="mt-4 space-y-4">
+      <p class="flex gap-4 items-center">
+        ${vids.authors[0].profile_name}
+        <span
+          ><img
+            class="w-4 h-4"
+            src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png"
+            alt=""
+            srcset=""
+        /></span>
+      </p>
+      <p>${vids.others.views} Views</p>
+    </div>
+  </div>
     `;
     videoContainer.append(videoCard);
   });
